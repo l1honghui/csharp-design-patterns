c933483ad69c5d7f735db2e43df728e2668fd9ec
宏辉 李 <lihonghui1012@outlook.com>
2019-04-22 16:17:39 +0800

    简单工厂测试

--- src/csharp-design-patterns/Program.cs
+++ src/csharp-design-patterns/Program.cs
@@ -1,21 +1,41 @@
-﻿using csharp_design_patterns.design_patterns.singleton;
+﻿using csharp_design_patterns.design_patterns;
+using csharp_design_patterns.design_patterns.simple_factory;
+using csharp_design_patterns.design_patterns.singleton;
 using System;
 
 namespace csharp_design_patterns
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            write(test.Instance.GetHashCode());
-            write(test.Instance.GetHashCode());
+            TestSingleton();
             Console.ReadKey();
         }
 
-        static void write(object obj) => Console.WriteLine(obj);
+        private static void write(object obj)
+        {
+            Console.WriteLine(obj);
+        }
 
+        private static void TestSingleton()
+        {
+            write(Singleton.Instance.GetHashCode());
+            write(Singleton.Instance.GetHashCode());
+            write(SingletonBase<test>.Instance.GetHashCode());
+            write(SingletonBase<test>.Instance.GetHashCode());
+        }
+
+        private static void TestSimpleFactory()
+        {
+            IProduct huawei = SimpleFactory.GetProduct("huawei");
+            IProduct iphone = SimpleFactory.GetProduct("iphone");
+            IProduct samsung = SimpleFactory.GetProduct("samsung");
+            write("huawei customer buy " + huawei.GetProductName());
+            write("iphone customer buy " + iphone.GetProductName());
+            write("samsung customer buy " + samsung.GetProductName());
+        }
     }
-
 
     class test : SingletonBase<test>
     {
