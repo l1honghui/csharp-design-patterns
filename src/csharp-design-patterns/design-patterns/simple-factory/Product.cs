73105b01ceef43e55ac7080fdcc05d1bfaa9d359
宏辉 李 <lihonghui1012@outlook.com>
2019-04-22 16:10:57 +0800

    简单工厂

---
+++ src/csharp-design-patterns/design-patterns/simple-factory/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharp_design_patterns.design_patterns.simple_factory
+{
+    /// <summary>
+    /// 提供给客户选择的产品
+    /// </summary>
+    public interface IProduct
+    {
+        string GetProductName();
+    }
+
+    public class Iphone : IProduct
+    {
+        public string GetProductName() => nameof(Iphone);
+    }
+
+    public class HuaWei : IProduct
+    {
+        public string GetProductName() => nameof(HuaWei);
+    }
+
+    public class Samsung : IProduct
+    {
+        public string GetProductName() => nameof(Samsung);
+    }
+}
---
+++ src/csharp-design-patterns/design-patterns/simple-factory/SimpleFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharp_design_patterns.design_patterns.simple_factory
+{
+    public class SimpleFactory
+    {
+        /// <summary>
+        /// 工厂类，封装了创建对象的逻辑代码，
+        /// 优点：解耦了客户和产品的直接关系，通过工厂来管理，客户要什么我给你什么，代码简洁，可复用
+        /// 缺点：工厂和产品高度耦合，如果需要添加一个新的产品就需要改动工厂类代码
+        /// </summary>
+        /// <param name="productName"></param>
+        /// <returns></returns>
+        public static IProduct GetProduct(string productName)
+        {
+            IProduct product = null;
+            switch (productName.ToLower())
+            {
+                case "iphone":
+                    product = new Iphone();
+                    break;
+                case "huawei":
+                    product = new HuaWei();
+                    break;
+                case "samsung":
+                    product = new Samsung();
+                    break;
+                default:
+                    throw new Exception("产品不在提供范围内");
+            }
+            return product;
+        }
+    }
+}
