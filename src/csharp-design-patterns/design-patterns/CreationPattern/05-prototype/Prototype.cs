914e6a5416a532fd0c9fdf311e1167fd6632b11e
宏辉 李 <lihonghui1012@outlook.com>
2019-06-15 15:16:01 +0800

    添加原型模式

---
+++ src/csharp-design-patterns/design-patterns/CreationPattern/05-prototype/Prototype.cs
@@ -0,0 +1,16 @@
+﻿namespace csharp_design_patterns.design_patterns.CreationPattern._05_prototype
+{
+    /// <summary>
+    /// 抽象类 定义基本行为
+    /// </summary>
+    public abstract class Prototype
+    {
+        public abstract void Eat();
+
+        /// <summary>
+        /// 克隆方法生成一个新的自己
+        /// </summary>
+        /// <returns></returns>
+        public abstract Prototype Clone();
+    }
+}
---
+++ src/csharp-design-patterns/design-patterns/CreationPattern/05-prototype/VirusPrototype.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace csharp_design_patterns.design_patterns.CreationPattern._05_prototype
+{
+    class VirusPrototype : Prototype
+    {
+        public override Prototype Clone()
+        {
+            // Prototype模式中的Clone方法可以利用.NET中的Object类的MemberwiseClone()方法或者序列化来实现深拷贝。
+            return (Prototype)MemberwiseClone();
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine($"{nameof(VirusPrototype)} was eating");
+        }
+
+    }
+}
